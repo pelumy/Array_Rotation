6668e4ab08ea927de39289c325e36231facef1d3
Itunu Raimi <chavitunu@gmail.com>
2020-08-05 12:38:28 +0200

    going to other questions to save time

---
+++ array_rotate.py
@@ -0,0 +1,6 @@
+def rotate_array(lst, n):
+    # return initial array
+    for i in range(0, len(lst)):
+        return lst[i]
+    # rotate array n times towards right
+    print(rotate_array([1,2,3,4,5,6],3))
